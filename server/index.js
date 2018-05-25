5160a30207e8ca01e51eb173eb164a4d5f7a6370
mattl <matt.lee.personal@gmail.com>
2018-05-25 15:33:38 -0500

    updated LOTS of stuff in package.json

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-var items = require('../database-mongo');
+const items = require('../database-mongo');
 
-var app = express();
+const app = express();
 
 // UNCOMMENT FOR REACT
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(`${__dirname}/../react-client/dist`));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
     if(err) {
       res.sendStatus(500);
     } else {
@@ -23,7 +23,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
 
