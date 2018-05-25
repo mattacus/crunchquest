f0dcb7fb71334d759675a91a4adbec47b7d18158
mattl <matt.lee.personal@gmail.com>
2018-05-25 14:31:41 -0500

    uncomment server dependencies

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mongo');
 
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
