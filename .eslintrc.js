eca7a8abe2ebd9c1dba9923f97b473ef55f63841
mattl <matt.lee.personal@gmail.com>
2018-05-26 10:30:33 -0500

    update eslint, remove jquery

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  "extends": "airbnb-base"
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "no-unused-vars": 0,
+  }
 };
--- server/index.js
+++ server/index.js
@@ -21,6 +21,6 @@ app.get('/items', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('listening on port 3000!');
+  console.log('listening on port: ', PORT);
 });
 
