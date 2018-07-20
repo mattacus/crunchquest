d95a118a517a44d68236cc99252a85751610a63a
mattl <matt.lee.personal@gmail.com>
2018-07-20 12:25:23 -0500

    improvements to map results correlator

--- database-mongo/mapDataCorrelator.js
+++ database-mongo/mapDataCorrelator.js
@@ -13,8 +13,38 @@ let correlateMapData = (placeResults, company, last) => {
     let { searchDetailsCache } = placeResults[i];
 
     if (searchDetailsCache.website) {
-      let mapsName = searchDetailsCache.website.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').replace(/.com.*/, '');
-      let crunchName = company.homepage_url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').replace(/.com.*/, '');
+
+      // lowercase both urls
+      let mapsName = searchDetailsCache.website.toLowerCase();
+      let crunchName = company.homepage_url.toLowerCase();
+
+      // replace http, www
+      mapsName = mapsName.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
+      crunchName = crunchName.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
+
+      // replace .com
+      mapsName = mapsName.replace(/.com.*/, '');
+      crunchName = crunchName.replace(/.com.*/, '');
+
+      // replace other subdomains
+      mapsName = mapsName.replace(/.org.*/, '');
+      crunchName = crunchName.replace(/.org.*/, '');
+      mapsName = mapsName.replace(/.bar.*/, '');
+      crunchName = crunchName.replace(/.bar.*/, '');
+      mapsName = mapsName.replace(/.ca.*/, '');
+      crunchName = crunchName.replace(/.ca.*/, '');
+
+      // remove trailing characters
+      let mapsNameLast = mapsName.slice(-1);
+      let crunchNameLast = crunchName.slice(-1);
+      if (mapsNameLast === '/') {
+        mapsName = mapsName.substring(0, mapsName.length - 1);
+      }
+      if (crunchNameLast === '/') {
+        crunchName = crunchName.substring(0, crunchName.length - 1);
+      }
+
+
       logger.info(`Website retrieved for ${company.name}: ${searchDetailsCache.website}`);
       logger.info('Maps website post-regex: ', mapsName);
       logger.info('Crunchbase website: ', company.homepage_url);
