dcc81024d677cc5701baa761c2362ad41cadafcc
mattl <matt.lee.personal@gmail.com>
2018-07-20 11:40:17 -0500

    optimizations for heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+let CompressionPlugin = require('compression-webpack-plugin');
 
 const SRC_DIR = path.join(__dirname, '/react-client/src');
 const DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
+  mode: (process.env.NODE_ENV === 'production' ? 'production' : 'development'),
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
@@ -26,4 +29,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0,
+    }),
+  ],
 };
